595719e4ad6207ae1f6274ccd102b7aa38d8017c
Salvador Rodríguez-Gómez <salrodgom@upo.es>
2017-04-21 08:58:50 +0200

    modified:   lib/fff_lammps/in.lmp 	modified:   test_adsorption.sh

--- test_adsorption.sh
+++ test_adsorption.sh
@@ -2,6 +2,6 @@
 temperature=85
 structure="ZIF-8.cif"
 for pressure in 17500 55000 60000 10 ; do
- bash lammps_raspa_interface.sh $structure $temperature $pressure argon 2 &
+ bash lammps_raspa_interface.sh $structure $temperature $pressure argon 2
 done
 exit 0 
